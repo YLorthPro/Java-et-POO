0c2bded13c50caddb5598b1b30f8edf08a480dfd
YLorthPo <yann.lorthioir@bstorm.be>
2023-06-20 16:07:30 +0200

    Distributeur de boisson v1

---
+++ src/DistributeurDeBoissons.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class DistributeurDeBoissons {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String choixUtilisateur;
+        String autreChoix;
+
+        boolean repeat;
+
+        do {
+            System.out.println("Quelle boisson voulez vous? 1.Cola - 2.Eau plate - 3.Eau gazeuse");
+            choixUtilisateur = sc.nextLine();
+
+            switch (choixUtilisateur){
+                case "1":
+                    System.out.println("Voici un cola!");
+                    break;
+                case "2":
+                    System.out.println("Voici une eau plate!");
+                    break;
+                case "3":
+                    System.out.println("Voici une eau gazeuse!");
+                    break;
+                default:
+                    System.out.println("Boisson non connue");
+            }
+
+            do {
+                System.out.println("Voulez-vous une autre boisson? Oui (O), non (N)");
+                autreChoix = sc.nextLine();
+
+                if("o".equals(autreChoix.toLowerCase()))
+                    repeat=true;
+                else
+                    repeat=false;
+            }while(!autreChoix.toLowerCase().equals("o") && !autreChoix.toLowerCase().equals("n"));
+
+        }while (repeat);
+
+        System.out.println("Au revoir!");
+
+    }
+}
