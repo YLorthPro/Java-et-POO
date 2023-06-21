af33535f8ff86e871ef8cbf29bbac1b33bd91407
YLorthPo <yann.lorthioir@bstorm.be>
2023-06-21 14:06:29 +0200

    Distributeur de boisson v1

--- src/DistributeurDeBoissons.java
+++ src/DistributeurDeBoissons.java
@@ -5,17 +5,24 @@ public class DistributeurDeBoissons {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Boisson maBoisson =new Boisson();
+
+        Boisson maBoisson = new Boisson();
         maBoisson.nom="cola";
         maBoisson.quantite=3;
 
-        System.out.println(maBoisson);
+        Boisson maBoisson2 = new Boisson();
+        maBoisson2.nom="Eau plate";
+        maBoisson2.quantite=2;
 
+        Boisson maBoisson3 = new Boisson();
+        maBoisson3.nom="Eau gazeuse";
+        maBoisson3.quantite=5;
 
         String choixUtilisateur;
         String autreChoix;
 
         boolean repeat;
+
 
         do {
             System.out.println("Quelle boisson voulez vous? 1.Cola - 2.Eau plate - 3.Eau gazeuse");
@@ -23,13 +30,25 @@ public class DistributeurDeBoissons {
 
             switch (choixUtilisateur){
                 case "1":
-                    System.out.println("Voici un cola!");
+                    if(maBoisson.quantite>0){
+                        System.out.println("Voici un "+ maBoisson.nom);
+                        System.out.println("Quantité restante: "+ (--maBoisson.quantite));;
+                    } else
+                        System.out.println("Nous n'avons plus de "+ maBoisson.nom);
                     break;
                 case "2":
-                    System.out.println("Voici une eau plate!");
+                    if(maBoisson2.quantite>0){
+                        System.out.println("Voici un "+maBoisson2.nom);
+                        System.out.println("Quantité restante: "+ (--maBoisson2.quantite));;
+                    } else
+                        System.out.println("Nous n'avons plus de "+maBoisson2.nom);
                     break;
                 case "3":
-                    System.out.println("Voici une eau gazeuse!");
+                    if(maBoisson3.quantite>0){
+                        System.out.println("Voici un "+maBoisson3.nom);
+                        System.out.println("Quantité restante: "+ (--maBoisson3.quantite));;
+                    } else
+                        System.out.println("Nous n'avons plus de "+maBoisson3.nom);
                     break;
                 default:
                     System.out.println("Boisson non connue");
@@ -45,7 +64,10 @@ public class DistributeurDeBoissons {
                     repeat=false;
             }while(!autreChoix.toLowerCase().equals("o") && !autreChoix.toLowerCase().equals("n"));
 
-        }while (repeat);
+        }while (repeat && (maBoisson.quantite + maBoisson2.quantite+ maBoisson3.quantite >0));
+
+        if(maBoisson.quantite + maBoisson2.quantite+ maBoisson3.quantite==0)
+            System.out.println("Plus de boissons disponibles");
 
         System.out.println("Au revoir!");
 
