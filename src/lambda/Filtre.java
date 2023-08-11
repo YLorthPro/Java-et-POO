2b19fa4624b1a192af7198b7b027d980687bf4e7
YLorthPro <yann.lorthioir@bstorm.be>
2023-08-11 12:24:16 +0200

    parking + lambda

--- src/lambda/Filtre.java
+++ src/lambda/Filtre.java
@@ -2,6 +2,6 @@ package lambda;
 
 public interface Filtre {
 
-    Boolean isImpair (int n) ;
+    boolean isImpair (int n) ;
 
 }
--- src/lambda/Lambda.java
+++ src/lambda/Lambda.java
@@ -1,19 +1,40 @@
 package lambda;
 
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
 public class Lambda {
     public static void main (String [] args) {
 
         int [] tab = {1, 24, -22563, 12349, -38, 56, -3236 } ;
         System.out.print ("-- Les nombres impairs de tab : ") ;
         affichage(tab, elements -> elements%2 != 0) ;
+
+        System.out.print ("-- Les positifs de tab : ") ;
+        affichage (tab, e -> e > 0) ;
+
+        System.out.print ("-- Les négatifs de tab : ") ;
+        affichage (tab, e -> e < 0) ;
+
+        System.out.print ("-- Les pairs de tab : ") ;
+        affichage (tab, e -> e%2 == 0) ;
+
+        System.out.print ("-- Les carrés de tab : ") ;
+        afficherCarre (tab, e -> e*e); ;
     }
 
-    public static void affichage (int [] tab, Filtre f){
+    public static void affichage (int [] tab, Predicate<Integer> f){
         for (int val : tab){
-            if (f.isImpair (val))
+            if (f.test(val))
                 System.out.print (val + " * ") ;
         }
             System.out.println() ;
+    }
+
+    public static void afficherCarre(int[] tab, UnaryOperator<Integer> f){
+        for (int i : tab) {
+            System.out.print(f.apply(i)+" * ");
+        }
     }
 
 }
